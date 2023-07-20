5157eb20c7aa1b0fc4beba6318fd8aecc089b829
SShinThant <samuelminlay@gmail.com>
2023-07-20 22:22:42 +0630

    Player Movement

---
+++ Assets/Scripts/WeaponItem.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SST
+{
+    [CreateAssetMenu(menuName = "Items/Weapon Item")]
+    public class WeaponItem : Item
+    {
+        public GameObject modelPrefab;
+        public bool isUnarmed;
+
+        [Header("Idle Animations")]
+        public string Right_Hand_Idle;
+        public string Left_Hand_Idle;
+
+        [Header("Attack Animations")]
+        public string OH_Light_Attack_1;
+        public string OH_Light_Attack_2;
+        public string OH_Heavy_Attack_1;
+    }
+}
